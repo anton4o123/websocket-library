46485287234a24256723a9ae9fd076c9fe8e15ac
Anton Georgiev <anton4o123@abv.bg>
2014-08-06 17:08:15 +0300

    The echo client is ready

--- echo/echocli.cc
+++ echo/echocli.cc
@@ -5,14 +5,20 @@ int main(int argc, char const *argv[])
 	srand((unsigned)time(NULL));
 	WebSocket wb(WEB_WS);
 	char *buf=(char*)malloc(65650*sizeof(char));
-	string haha;
+	string echo_str;
 
 	wb.connect_to(argv[1]);
 
-	cin >> haha;
-	wb.send_text(haha);
-	wb.receive(buf);
-	cout << buf << endl;
+	while(true) {
+		cin >> echo_str;
+		if(!cin) {
+			break;
+		}
+		wb.send_text(echo_str);
+		wb.receive(buf);
+		cout << buf << endl;
+	}
+
 	free(buf);
 	
 	wb.disconnect();
